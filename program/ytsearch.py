142c948d49dd8ef769e0a56401d28a35fbfd97f2
Rahid2003 <104177696+Rahid2003@users.noreply.github.com>
2022-06-06 01:02:24 +0400

    update ytsearch.py

--- program/ytsearch.py
+++ program/ytsearch.py
@@ -8,24 +8,24 @@ from youtube_search import YoutubeSearch
 @Client.on_message(command(["search", f"search@{BOT_USERNAME}"]))
 async def ytsearch(_, message: Message):
     if len(message.command) < 2:
-        return await message.reply_text("**ʙᴀɴᴀ ᴠɪᴅᴇᴏ ɪsᴍɪ ᴠᴇʀ !**")
+        return await message.reply_text("**Mənə videonun adını yazın!**")
     query = message.text.split(None, 1)[1]
-    m = await message.reply_text("🔎 **ᴀʀᴀɴɪʏᴏʀ . . .**")
+    m = await message.reply_text("🔎 **Axtarılır. . .**")
     results = YoutubeSearch(query, max_results=5).to_dict()
     text = ""
     for i in range(5):
         try:
-            text += f"🏷 **ɪsɪᴍ:** __{results[i]['title']}__\n"
-            text += f"⏱ **sᴜʀᴇ:** `{results[i]['duration']}`\n"
-            text += f"👀 **ɢᴏʀᴜɴᴛᴜʟᴇᴍᴇ:** `{results[i]['views']}`\n"
-            text += f"📣 **ᴋᴀɴᴀʟ:** {results[i]['channel']}\n"
-            text += f"🔗 **ʟɪɴᴋ:** https://www.youtube.com{results[i]['url_suffix']}\n\n"
+            text += f"🏷 **Ad:** __{results[i]['title']}__\n"
+            text += f"⏱ **Müddət:** `{results[i]['duration']}`\n"
+            text += f"👀 **Baxış sayı:** `{results[i]['views']}`\n"
+            text += f"📣 **Kanal:** {results[i]['channel']}\n"
+            text += f"🔗 **Link:** https://www.youtube.com{results[i]['url_suffix']}\n\n"
         except IndexError:
             break
     await m.edit_text(
         text,
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("🗑 ᴋᴀᴘᴀᴛ", callback_data="cls")]]
+            [[InlineKeyboardButton("🗑 Bağla", callback_data="cls")]]
         ),
     )
