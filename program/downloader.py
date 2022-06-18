87b7540199a2b8ec9a922032b8fa31697e19509b
Rahid2003 <104177696+Rahid2003@users.noreply.github.com>
2022-06-18 17:17:02 +0400

    update downloader.py

--- program/downloader.py
+++ program/downloader.py
@@ -72,8 +72,8 @@ def song(_, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"• uploader @{bn}"
-        host = str(info_dict["uploader"])
+        rep = f"• Yükləyici @{bn}"
+        host = str(info_dict["Yükləyici"])
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(float(dur_arr[i])) * secmul
